ac2114aa0aad4dc376b2b64890627b0021856e7c
ehmurray8 <ehmurray8@gmail.com>
2018-03-26 23:11:09 -0400

    Added batch script.

---
+++ convert_hands.cmd
@@ -0,0 +1,1 @@
+python app_nogui.py
--- setup.py
+++ setup.py
@@ -1,2 +1,19 @@
-import setuptools
+from setuptools import setup
 
+setup(
+    name='igniton_hand_converter',
+    version='0.0.1',
+    description='Convert Ignition Hand History to PokerStars Hand History.',
+    url='https://github.com/ehmurray8/ignition-hand-converter.git',
+    author='Emmet Murray',
+    author_email='ehmurray8@gmail.com',
+    packages=['igconverter'],
+    entry_points={
+        'console_scripts': ['igconverter=igconverter.app_nogui:main']
+    },
+    install_requires=[
+        ''
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
